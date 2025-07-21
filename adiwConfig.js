cf65cc3e9ffb12fc31323fa5350bc418fa10c438
VexxuzzZ <rizkybayuaji267@gmail.com>
2025-07-21 14:26:44 +0700

    Add files via upload

---
+++ adiwConfig.js
@@ -0,0 +1,12 @@
+module.exports = {
+  'approval': {
+    'num': '6285773466911',
+    'text': "setujui",
+    'set': "👀 Harus mendapatkan persetujuan oleh creator script, Jika ingin memakai script ini",
+    'greet': "*Disetujui Oleh Creator, Silahkan Restart Panel Atau Run Ulang script* ☠️ "
+  },
+  'creatorScript': '62857734669112',
+  'filePath': "./all/approval",
+  'checkFilePath': "../RyuMa.js",
+  'codeToDetect': 'main();'
+};
